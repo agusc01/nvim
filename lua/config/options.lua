5b90bb7405101bb89189434490a3382c0ae06d0b
agus <33077305+agusc01@users.noreply.github.com>
2025-04-13 16:38:05 -0300

    Things for php

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,8 @@
 vim.g.mapleader = " "
+vim.g.lazyvim_php_lsp = "intelephense"
 
-vim.opt.hidden=true
-vim.opt.clipboard=unnamedplus
+vim.opt.hidden = true
+vim.opt.clipboard = unnamedplus
 
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
